33089d10581126cd0c45041dffe6209701145814
Your Name <you@example.com>
2021-02-08 12:51:03 +0000

    add

--- pj_blog/urls.py
+++ pj_blog/urls.py
@@ -1,35 +1,18 @@
-"""pj_blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
-from django.contrib.auth.models import Group
+from django.views.generic import ListView
+from blog.models import Post
 
-admin.site.site_title = '匿名ブログ 内部管理サイト'
-admin.site.site_header = '匿名ブログ 内部管理サイト'
+admin.site.site_title = '野村勇斗のブログ 内部管理サイト'
+admin.site.site_header = '野村勇斗のブログ 内部管理サイト'
 admin.site.index_title = 'メニュー'
-admin.site.unregister(Group)
-admin.site.disable_action('delete_selected')
 
-index_view = TemplateView.as_view(template_name="registration/index.html")
+index_view = ListView.as_view(template_name="blog/post_list.html", model=Post)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", login_required(index_view), name="index"),
+    path("", login_required(index_view), name="login"),
     path('blog/', include('blog.urls')),
     path('', include("django.contrib.auth.urls")),
 ]
