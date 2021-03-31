51ac88fbdf1691e8ee71c8ce70078e352c9adb5f
野村勇斗 <nomurahayato@nomuraisamutonoMacBook-Pro.local>
2021-03-31 16:55:58 +0900

    A

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -111,8 +111,3 @@ AUTH_USER_MODEL = 'registration.User'
 FRONTEND_URL = 'http://127.0.0.1:8000/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yongdouyecun@gmail.com'
-EMAIL_HOST_PASSWORD = 'adgjm135'
