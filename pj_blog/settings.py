0863712639c00e62a0a0b031b7c8417981ce0fac
野村勇斗 <nomurahayato@nomuraisamutonoMacBook-Pro.local>
2021-04-08 22:10:45 +0900

    A

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -125,4 +125,4 @@ EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yongdouyecun@gmail.com'
-EMAIL_HOST_PASSWORD = 'adgjm135'
+EMAIL_HOST_PASSWORD = 'bwantklkayhaqmjc'
