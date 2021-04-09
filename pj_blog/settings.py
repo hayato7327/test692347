4dd292bffaf83008a9d6c74a68475130d6f9da78
野村勇斗 <nomurahayato@nomuraisamutonoMacBook-Pro.local>
2021-04-09 23:02:27 +0900

    A

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'このリポジトリでは非公開'
-EMAIL_HOST_PASSWORD = 'このリポジトリでは非公開'
+EMAIL_HOST_USER = 'yongdouyecun@gmail.com'
+EMAIL_HOST_PASSWORD = 'bwantklkayhaqmjc'
